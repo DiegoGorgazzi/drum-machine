d6b98d21bdd146360654b597681d245920cdf0e6
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-03-24 11:47:50 +0100

    Set up DrumMachine component function

---
+++ src/components/DrumMachine.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./DrumMachine.css";
+
+
+const drumMachine = (props) => {
+    return (
+        <div id="drum-machine">
+            
+
+
+        </div>
+    )
+};
+
+export default drumMachine;
