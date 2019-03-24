5b9a8290dc686376f2288d7c6392dc08e507f725
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-03-24 12:01:15 +0100

    Add DrumMachine component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DrumMachine from "./components/DrumMachine";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+          <DrumMachine />
 
 
       </div>
