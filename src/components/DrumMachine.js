b732b6c77f04234cfb57de240fde552707b252e7
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-04-01 21:04:46 +0200

    Add divs per specs

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -6,16 +6,22 @@ const drumMachine = (props) => {
     return (
         <div id="drum-machine">
           <div id="display">
-            <audio
-              id="audio"
-              src="https://freesound.org/data/previews/203/203567_3660887-lq.mp3"
-            ></audio>
-            <button
-              onClick= {props.clickIt}
-              value = "https://freesound.org/data/previews/203/203567_3660887-lq.mp3"
-              >
-                Play 1
-            </button>
+
+            <div className="drum-pad"
+              id="Crash Cymbal">
+              <button
+                onClick= {props.clickIt}
+                value = "https://freesound.org/data/previews/203/203567_3660887-lq.mp3"
+                >
+                  Q
+                  <audio
+                    className="clip"
+                    id="Q"
+                    src="https://freesound.org/data/previews/203/203567_3660887-lq.mp3"
+                    >
+                  </audio>
+              </button>
+            </div>
 
           </div>
         </div>
