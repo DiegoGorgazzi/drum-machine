add3b96fe86ac5b0481224d675af8275ac8d13eb
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-04-03 19:13:53 +0200

    Add all other audio elements

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -9,7 +9,7 @@ const drumMachine = (props) => {
           <div id="display">
 
             <button className="drum-pad"
-              id="Crash Cymbal"
+              id="Crash-Cymbal"
               onClick= {props.clickIt}
               >
               Q
@@ -24,7 +24,7 @@ const drumMachine = (props) => {
             </button>
 
             <button className="drum-pad"
-              id="Small Tom"
+              id="Small-Tom"
               onClick= {props.clickIt}
               >
               W
@@ -37,6 +37,122 @@ const drumMachine = (props) => {
               </audio>
 
             </button>
+
+            <button className="drum-pad"
+              id="Bass-kick-Drum"
+              onClick= {props.clickIt}
+              >
+              E
+
+              <audio
+                className="clip"
+                id="E"
+                src="https://freesound.org/data/previews/25/25649_48671-lq.mp3"
+                >
+              </audio>
+
+            </button>
+
+            <button className="drum-pad"
+                id="Medium-Tom"
+                onClick= {props.clickIt}
+                >
+                A
+
+                <audio
+                  className="clip"
+                  id="A"
+                  src="https://freesound.org/data/previews/46/46554_394391-lq.mp3"
+                  >
+                </audio>
+
+            </button>
+
+            <button className="drum-pad"
+                id="Ride-Cymbal"
+                onClick= {props.clickIt}
+                >
+                S
+
+                <audio
+                  className="clip"
+                  id="S"
+                  src="https://freesound.org/data/previews/13/13250_36719-lq.mp3"
+                  >
+                </audio>
+
+            </button>
+
+            <button className="drum-pad"
+                id="Hi-Hat"
+                onClick= {props.clickIt}
+                >
+                D
+
+                <audio
+                  className="clip"
+                  id="D"
+                  src="https://freesound.org/data/previews/165/165321_3008343-lq.mp3"
+                  >
+                </audio>
+
+            </button>
+
+            <button className="drum-pad"
+                id="Snare-Drum"
+                onClick= {props.clickIt}
+                >
+                Z
+
+                <audio
+                  className="clip"
+                  id="Z"
+                  src="https://freesound.org/data/previews/270/270156_1125482-lq.mp3"
+                  >
+                </audio>
+
+            </button>
+
+            <button className="drum-pad"
+                id="Floor-Tom"
+                onClick= {props.clickIt}
+                >
+                X
+
+                <audio
+                  className="clip"
+                  id="X"
+                  src="https://freesound.org/data/previews/41/41948_433684-lq.mp3"
+                  >
+                </audio>
+
+            </button>
+
+            <button className="drum-pad"
+                id="Crash-Cymbal-2"
+                onClick= {props.clickIt}
+                >
+                C
+
+                <audio
+                  className="clip"
+                  id="C"
+                  src="https://freesound.org/data/previews/219/219612_3008343-lq.mp3"
+                  >
+                </audio>
+
+            </button>
+
+
+
+
+
+
+
+
+
+
+
 
           </div>
         </div>
