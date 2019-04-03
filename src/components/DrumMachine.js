d820298e6ebce3d461bd7152b68dea2959193e72
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-04-03 18:32:43 +0200

    Replace drum-pad div with button

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -3,25 +3,40 @@ import "./DrumMachine.css";
 
 
 const drumMachine = (props) => {
+
     return (
         <div id="drum-machine">
           <div id="display">
 
-            <div className="drum-pad"
-              id="Crash Cymbal">
-              <button
-                onClick= {props.clickIt}
-                value = "https://freesound.org/data/previews/203/203567_3660887-lq.mp3"
+            <button className="drum-pad"
+              id="Crash Cymbal"
+              onClick= {props.clickIt}
+              >
+              Q
+
+              <audio
+                className="clip"
+                id="Q"
+                src="https://freesound.org/data/previews/203/203567_3660887-lq.mp3"
                 >
-                  Q
-                  <audio
-                    className="clip"
-                    id="Q"
-                    src="https://freesound.org/data/previews/203/203567_3660887-lq.mp3"
-                    >
-                  </audio>
-              </button>
-            </div>
+              </audio>
+
+            </button>
+
+            <button className="drum-pad"
+              id="Small Tom"
+              onClick= {props.clickIt}
+              >
+              W
+
+              <audio
+                className="clip"
+                id="W"
+                src="https://freesound.org/data/previews/416/416292_8218607-lq.mp3"
+                >
+              </audio>
+
+            </button>
 
           </div>
         </div>
