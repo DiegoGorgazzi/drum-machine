9fa99562c97b611961ac08493c100c100f009113
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-04-09 22:26:22 +0200

    Remove "value" from event handlers

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ class App extends Component {
     const inOurKeys = ourKeys.includes(x);
       if (inOurKeys) {
       audios = document.getElementById(x);
-      values = document.getElementById(x).getAttribute("src");
 
       drumPadNames=  document.getElementById(x).parentNode.id;
 
@@ -36,7 +35,6 @@ class App extends Component {
         display: drumPadNames,
         play: !this.state.play },
         () => {
-          audios.src = values;
           audios.play();
           audios.currentTime = 0;
       });
@@ -51,14 +49,12 @@ class App extends Component {
 
   clickHandler = (event) => {
     const audio = event.target.firstElementChild
-    const value = event.target.firstElementChild.src
     const drumPadName = event.target.id
 
     this.setState({
       display: drumPadName,
       play: !this.state.play },
       () => {
-        audio.src = value;
         audio.play();
         audio.currentTime = 0;
     },
