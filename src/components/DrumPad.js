12f94ebec4297ca5c9d836b96b04d5b15905d3a1
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-04-11 21:42:58 +0200

    Add DrumPad component

---
+++ src/components/DrumPad.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {drumPadList} from "../shared/drumPadList";
+
+
+const drumPad = (props) => drumPadList.map((item, index) =>
+
+       {return (
+         <button className="drum-pad"
+          id = {item.padName}
+          onClick= {props.clickIt}
+          key={index}
+          >
+          {item.audioIdKey}
+
+          <audio
+            className="clip"
+            id={item.audioIdKey}
+            src={item.audioSrc}
+                            >
+          </audio>
+
+        </button>
+        )
+      });
+
+
+export default drumPad;
